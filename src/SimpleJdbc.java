0197d624b0dec2ccb88e3d9991c80af7d95e50e4
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-11-08 20:17:46 +0100

    Ændret SimpleJdbc til at connect til en database kaldet Zealand, derefter slettet jeg nr 4 og lavet en ny iterate der passere til min forspørgsel

--- src/SimpleJdbc.java
+++ src/SimpleJdbc.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class SimpleJdbc {
     public static void main(String[] args) throws SQLException {
 
-        System.out.println("\n****** \nProgram til hentning af landedata fra world.sql databasen. \n****** ");
+        System.out.println("\n****** \nProgram til hentning af landedata fra Zealand.sql databasen. \n****** ");
 
         // #1. Connect to the database
         Connection connection = null;
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/world?serverTimezone=UTC",
+                "jdbc:mysql://localhost:3306/Zealand?serverTimezone=UTC",
                 "root",
                 "CodeWarrior8"
         );
@@ -20,9 +20,9 @@ public class SimpleJdbc {
 
         // ** Med Scanner kan vi få brugeren til at indtaste, hvilket land, de vil se info om
          Scanner scanner = new Scanner(System.in);
-         System.out.print("Hvilket land vil du se oplysninger om? ±");
-         String landeNavn = scanner.nextLine();
-         String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + landeNavn + "';";
+         System.out.print("Hvilket student vil du se oplysninger om? ±");
+         String student = scanner.nextLine();
+         String mitQuery ="SELECT studerende.fornavn, studerende.efternavn FROM studerende WHERE fornavn LIKE '" + student + "';";
 
         // ** Uden scanner kan vi kun vise det land, som vi harcoder i query-et
         // String mitQuery = "SELECT * FROM world.country WHERE name LIKE 'D%';";
@@ -38,12 +38,7 @@ public class SimpleJdbc {
         int i = 0;
         // #4. Iterate through the result and print the results (vi har måske flere resultater end 1, derfor vil en løkke læse alle rækker ud fra resultSettet)
             while (resultSet.next())
-                i++;
-            System.out.println(resultSet.getString(1) + " " +
-                    resultSet.getString(2) + ", " +
-                    resultSet.getString(3) + ", " +
-                    "Population: " +
-                    resultSet.getString("Population"));
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
 
         // #5. Close the connection (always!)
         connection.close();
